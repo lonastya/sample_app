ce69c941fda58668d4a210d79ed7bb5624d619ff
lonastya <lonastya@gmail.com>
2017-01-25 22:19:37 +0200

    Finish user edit, update, index, and destroy

--- Gemfile
+++ Gemfile
@@ -5,6 +5,9 @@ gem 'rails', '4.2.1'
 gem 'bootstrap-sass', '2.3.2.0'
 gem 'bcrypt'
 gem 'pry-rails'
+gem 'faker'
+gem 'will_paginate'
+gem 'bootstrap-will_paginate'
 
 group :development, :test do
 	gem 'sqlite3', '~>1.3','>=1.3.11'
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,10 +4,11 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
+    user = User.find_by_email(params[:email].downcase)
+    if user && user.authenticate(params[:password])
+      session[:user] = user.id
       sign_in user
-      redirect_to user
+      redirect_back_or user
     else
       flash.now[:error] = 'Invalid email/password combination'
       render 'new'
@@ -16,6 +17,6 @@ class SessionsController < ApplicationController
 
   def destroy
     sign_out
-    redirect_to root_url
+    redirect_to root_path
   end
 end
